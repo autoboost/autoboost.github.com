8dd10312b974e10b4ae8e6065d3a712ee721eb11
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-04 12:39:32 +0100

    Fixed menu on mobile

--- script.js
+++ script.js
@@ -33,3 +33,11 @@ function closePerson() {
     $("#team #personInfo").hide(500);
     selectedPerson = undefined;
 }
+
+function toggleMenu() {
+    $("#menu").toggle(500);
+    if ($("#menu-open-close").attr("name") === "menu")
+        $("#menu-open-close").attr("name", "close");
+    else
+        $("#menu-open-close").attr("name", "menu");
+}
