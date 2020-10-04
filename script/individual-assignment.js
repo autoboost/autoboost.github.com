c44f51665cd240a6a31f674a54cf843f8df3e453
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-05 00:59:09 +0100

    Improved assignments page

--- script/individual-assignment.js
+++ script/individual-assignment.js
@@ -1,3 +1,21 @@
 let urlParams = new URLSearchParams(window.location.search)
 let person = urlParams.get("person")
 
+let examplesNames = {
+    "afonso": {
+        "good": "Afonso Good",
+        "bad": "Afonso Bad"
+    },
+    "alexandre": {
+        "good": "Alexandre Good",
+        "bad": "Alexandre Bad"
+    },
+    "diogo": {
+        "good": "Diogo Good",
+        "bad": "Diogo Bad"
+    },
+    "pedro": {
+        "good": "Pedro Good",
+        "bad": "Pedro Bad"
+    }
+}
