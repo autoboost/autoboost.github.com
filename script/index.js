a2d2b9883550f07b5238fcc07f09291d93bd1d42
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-04 23:29:13 +0100

    Updated LinkedIn urls and Started developing individual assignments pages

---
+++ script/index.js
@@ -0,0 +1,51 @@
+let peopleInfo = {
+    "afonso": {
+        "name": "Afonso Nascimento",
+        "linkedin": "http://www.linkedin.com/in/afonso-nascimento-3407ba1b6/",
+        "personalPage": undefined
+    },
+    "alexandre": {
+        "name": "Alexandre Correia",
+        "linkedin": "https://www.linkedin.com/in/4lexcorrei4/",
+        "personalPage": "https://bitsys.tech"
+    },
+    "diogo": {
+        "name": "Diogo Gomes",
+        "linkedin": "https://www.linkedin.com/in/diogo-gomes-ab706b163/",
+        "personalPage": undefined
+    },
+    "pedro": {
+        "name": "Pedro Ferreira",
+        "linkedin": "javascript:;",
+        "personalPage": undefined
+    }
+};
+let peopleIDs = Object.keys(peopleInfo);
+let selectedPerson = undefined;
+
+function openPerson(id) {
+    if (selectedPerson !== id) {
+        peopleIDs.map(person => $("#team #person-" + person).removeClass("active"));
+        if (!selectedPerson)
+            $("#team #personInfo").show(500);
+        selectedPerson = id;
+        $("#team #person-" + id).addClass("active");
+
+        // set info
+        $("#team #personInfo #name").html(peopleInfo[id].name);
+    }
+}
+
+function closePerson() {
+    peopleIDs.map(person => $("#team #person-" + person).removeClass("active"));
+    $("#team #personInfo").hide(500);
+    selectedPerson = undefined;
+}
+
+function toggleMenu() {
+    $("#menu").toggle(500);
+    if ($("#menu-open-close").attr("name") === "menu")
+        $("#menu-open-close").attr("name", "close");
+    else
+        $("#menu-open-close").attr("name", "menu");
+}
---
+++ script/individual-assignment.js
@@ -0,0 +1,2 @@
+let urlParams = new URLSearchParams(window.location.search)
+let person = urlParams.get("person")
