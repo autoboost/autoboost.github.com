0bdd8e25240dc7acfaa2fe1719f8918d5651d776
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-06 20:23:51 +0100

    Almost done

--- infos/people.js
+++ infos/people.js
@@ -2,21 +2,25 @@ let peopleInfo = {
     "afonso": {
         "name": "Afonso Nascimento",
         "linkedin": "http://www.linkedin.com/in/afonso-nascimento-3407ba1b6/",
-        "personalPage": undefined
+        "personalPage": undefined,
+        "individualAssignment": "javascript:;"
     },
     "alexandre": {
         "name": "Alexandre Correia",
         "linkedin": "https://www.linkedin.com/in/4lexcorrei4/",
-        "personalPage": "https://bitsys.tech"
+        "personalPage": "https://bitsys.tech",
+        "individualAssignment": "javascript:;"
     },
     "diogo": {
         "name": "Diogo Gomes",
         "linkedin": "https://www.linkedin.com/in/diogo-gomes-ab706b163/",
-        "personalPage": undefined
+        "personalPage": undefined,
+        "individualAssignment": "javascript:;"
     },
     "pedro": {
         "name": "Pedro Ferreira",
         "linkedin": "https://www.linkedin.com/in/pedro-ferreira-59baa3116/",
-        "personalPage": undefined
+        "personalPage": undefined,
+        "individualAssignment": "javascript:;"
     }
 };
--- script/common.js
+++ script/common.js
@@ -5,3 +5,34 @@ function toggleMenu() {
     else
         $("#menu-open-close").attr("name", "menu");
 }
+
+let logged = false;
+
+function login() {
+    if (!logged) {
+        let pass = undefined;
+        while (pass === undefined) {
+            let tempPass = prompt("Enter the password");
+            if (tempPass === null) {
+                logged = false;
+                pass = "";
+            } else if (tempPass != "ipm2020")
+                alert("Wrong password, please try again");
+            else {
+                pass = tempPass;
+                logged = true;
+                $("#menu-login").html("Log Out&nbsp;<ion-icon name='lock-open'></ion-icon>");
+            }
+        }
+    } else {
+        $("#menu-login").html("Log In&nbsp;<ion-icon name='lock-closed'></ion-icon>");
+        logged = false;
+    }
+}
+
+function openURL(url, target) {
+    if (!logged)
+        alert("Please login");
+    else
+        window.open(url, target);
+}
