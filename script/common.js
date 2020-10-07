2556f9dde5e14480a19145f9f4e09d3e2e00342a
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-08 00:51:03 +0100

    Fixed menu bug

--- script/common.js
+++ script/common.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
-    $("#menu").toggle(500);
-    if ($("#menu-open-close").attr("name") === "menu")
-        $("#menu-open-close").attr("name", "close");
-    else
-        $("#menu-open-close").attr("name", "menu");
+    if (window.matchMedia("(max-width: 975px)").matches) {
+        $("#menu").toggle(500);
+        if ($("#menu-open-close").attr("name") === "menu")
+            $("#menu-open-close").attr("name", "close");
+        else
+            $("#menu-open-close").attr("name", "menu");
+    }
 }
