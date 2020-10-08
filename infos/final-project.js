9cf3e9b62575b8fee82f2c5ebaa9bd130dfe36ca
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-08 20:59:51 +0100

    Added final project deadlines

---
+++ infos/final-project.js
@@ -0,0 +1,32 @@
+let finalProjectStages = {
+    "01": {
+        "date": "12/10/2020",
+        "title": "Project Proposal",
+        "url": "javascript:;"
+    },
+    "02": {
+        "date": "19/10/2020",
+        "title": "User and Task analysis",
+        "url": "javascript:;"
+    },
+    "03": {
+        "date": "??/??/2020",
+        "title": "1st prototype",
+        "url": "javascript:;"
+    },
+    "04": {
+        "date": "??/??/2020",
+        "title": "Computational prototype",
+        "url": "javascript:;"
+    },
+    "05": {
+        "date": "??/??/2020",
+        "title": "Heuristic evaluation",
+        "url": "javascript:;"
+    },
+    "06": {
+        "date": "??/??/2020",
+        "title": "Evaluation results and presentation",
+        "url": "javascript:;"
+    }
+};
