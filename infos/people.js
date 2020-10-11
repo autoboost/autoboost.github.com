a0296e553eab46287f193c5e842030d6cc70d65e
Alexandre Correia <auto.boost.official@gmail.com>
2020-10-11 22:48:04 +0100

    Published stage 01 report

--- infos/people.js
+++ infos/people.js
@@ -3,24 +3,24 @@ let peopleInfo = {
         "name": "Afonso Nascimento",
         "linkedin": "http://www.linkedin.com/in/afonso-nascimento-3407ba1b6/",
         "personalPage": undefined,
-        "individualAssignment": "./individual-assignments/afonso-nascimento-53193.pdf"
+        "individualAssignment": "./ipm/individual-assignments/afonso-nascimento-53193.pdf"
     },
     "alexandre": {
         "name": "Alexandre Correia",
         "linkedin": "https://www.linkedin.com/in/4lexcorrei4/",
         "personalPage": "https://bitsys.tech",
-        "individualAssignment": "./individual-assignments/alexandre-correia-53298.pdf"
+        "individualAssignment": "./ipm/individual-assignments/alexandre-correia-53298.pdf"
     },
     "diogo": {
         "name": "Diogo Gomes",
         "linkedin": "https://www.linkedin.com/in/diogo-gomes-ab706b163/",
         "personalPage": undefined,
-        "individualAssignment": "./individual-assignments/diogo-gomes-52281.pdf"
+        "individualAssignment": "./ipm/individual-assignments/diogo-gomes-52281.pdf"
     },
     "pedro": {
         "name": "Pedro Ferreira",
         "linkedin": "https://www.linkedin.com/in/pedro-ferreira-59baa3116/",
         "personalPage": undefined,
-        "individualAssignment": "./individual-assignments/pedro-ferreira-52370.pdf"
+        "individualAssignment": "./ipm/individual-assignments/pedro-ferreira-52370.pdf"
     }
 };
